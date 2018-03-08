6e3304da4daffa35c4029ba4403dae91da5a3952
Ricardo Maes <ricmzn@gmail.com>
2018-03-08 11:05:52 -0300

    Adicionada ligação da API ao front-end

--- web/src/js/index.js
+++ web/src/js/index.js
@@ -2,5 +2,5 @@ import $ from "jquery";
 
 $(document).ready(() => {
     // Exibe mensagem de teste se a página for carregada corretamente
-    $("#msg").text("Página carregada com sucesso!");
+    $("#msg").text("API em uso: " + window.apiPath);
 });
